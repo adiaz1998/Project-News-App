dfd1b96215c256161059a7e40039daeb13f326d9
Alan <ajd299@scarletmail.rutgers.edu>
2021-10-06 04:45:44 -0400

    Fixed some code

--- user.py
+++ user.py
@@ -28,6 +28,7 @@ class User:
 
 
 def registerUser(db):
+    data = ""
     if request.method == 'POST' and request.form.get("firstName") and request.form.get("lastName") \
             and request.form.get("username") and request.form.get("password") and request.form.get("email"):
         first_name = request.form['firstName']
@@ -37,25 +38,25 @@ def registerUser(db):
         email = request.form['email']
 
         if User.validateIfFieldExist(db, username, "username"):
-            jsondata = {"message": "A user with the email already exists", "status": 400}
-            return render_template('signup-form.html', data=jsondata), 400
+            data = "A user with the email already exists"
+            return render_template('signup-form.html', data=data), 400
 
         if User.validateIfFieldExist(db, email, "email"):
-            jsondata = {"message": "A user with the email already exists", "status": 400}
-            return render_template('signup-form.html', data=jsondata), 400
+            data = "A user with the email already exists"
+            return render_template('signup-form.html', data=data), 400
 
         connection = db.connect()
         cursor = connection.cursor(pymysql.cursors.DictCursor)
         query = "INSERT INTO users VALUES (NULL, %s, %s, %s, %s, %s)"
         cursor.execute(query, (first_name, last_name, username, password, email,))
         connection.commit()
-        jsondata = {"message": "User created successfully.", "status": 201}
-        return render_template('signup-form.html', data=jsondata), 201
+        data = "User created successfully!"
+        return render_template('signup-form.html', data=data), 201
 
     elif request.method == 'POST':
-        jsondata = {"message": "Please fill out the form.", "status": 400}
-        return render_template('signup-form.html', data=jsondata), 400
+        data = "Please fill out the form!"
+        return render_template('signup-form.html', data=data), 400
 
     else:
-        jsondata = {"message": "The server has encountered a situation it does not know how to handle.", "status": "500"}
-        return render_template('signup-form.html', data=jsondata), 400
+        data = {"message": "The server has encountered a situation it does not know how to handle.", "status": "500"}
+        return render_template('signup-form.html', data=data), 400
